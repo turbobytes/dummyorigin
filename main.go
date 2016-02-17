42120831a3fafab9c01a158d28169db758ffab4d
Sajal Kayan <sajal@turbobytes.com>
2016-02-18 02:30:46 +0700

    add precise origin time for debugging

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 var pixel = []byte{71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 0, 0, 0, 0, 0, 255, 255, 255, 33, 249, 4, 1, 0, 0, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 1, 68, 0, 59}
@@ -17,6 +18,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	w.Header().Set("content-type", "image/png")
+	w.Header().Set("X-TB-time", time.Now().String())
 	w.Write(pixel)
 }
 
